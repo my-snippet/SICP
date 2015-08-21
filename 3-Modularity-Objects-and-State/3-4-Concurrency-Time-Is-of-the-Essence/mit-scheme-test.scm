b20ca2e0343811ade85451dccd22542b881729be
wikibootup <wikibootup@gmail.com>
2015-08-21 22:25:33 +0900

    mit scheme test

---
+++ 3-Modularity-Objects-and-State/3-4-Concurrency-Time-Is-of-the-Essence/mit-scheme-test.scm
@@ -0,0 +1,36 @@
+(load "modules/3/parallel.scm")
+
+(define (square x)
+  (* x x))
+
+(square 3)
+
+(display "dddddddddddd")
+(display (square 3))
+
+(stream-car (cons-stream 1 2))
+
+(define x 10)
+(parallel-execute (lambda () (set! x (* x x)))
+		  (lambda () (set! x (+ x 1))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
