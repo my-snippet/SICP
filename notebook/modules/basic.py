05b69c6e0956c0304be8e55f324abd1a6ccf79a4
wikibootup <wikibootup@gmail.com>
2015-09-09 23:03:11 +0900

    add module : modules/basic.py

--- notebook/modules/basic.py
+++ notebook/modules/basic.py
@@ -1,3 +1,6 @@
+from types import GeneratorType
+
+
 """
 1.
 """
@@ -44,11 +47,17 @@ def cons_stream(a, b):
 
 
 def display_stream(s):
-    if isinstance(s, tuple):
-        print(s[0])
-        return display_stream(next(s[1]))
-    else:
-        print(s)
+    def inner(s):
+        if isinstance(s, tuple):
+            print(s[0])
+            return display_stream((s[1]))
+        else:
+            print(s)
+
+    try:
+        inner(yield_stream(s))
+    except:
+        pass
 
 
 def stream_enumerate_interval(low, high):
@@ -58,8 +67,8 @@ def stream_enumerate_interval(low, high):
             stream_enumerate_interval(low+1, high))
     else:
         return high
-    
-    
+
+
 def stream_filter(pred, stream):
     if not isinstance(stream, tuple):
         return stream
@@ -76,16 +85,17 @@ def apply(op, sequence):
         return sequence[0]
     else:
         return op(sequence[0], apply(op, sequence[1:]))
-    
-    
-def nth_elements_extractor(sequences, index):
+
+
+def nth_elements_extractor(index, sequences):
     if len(sequences) == 0:
         return ()
     else:
         return (sequences[0][index],) + \
-            nth_elements_extractor(sequences[1:], index)
+            nth_elements_extractor(index, sequences[1:])
 
 
+""" For finite stream
 def stream_map(f, argstreams):
     if not isinstance(argstreams[0], tuple):
         return apply(f, argstreams)
@@ -96,3 +106,49 @@ def stream_map(f, argstreams):
                     f, 
                     tuple(map(lambda e: next(e), 
                               nth_elements_extractor(argstreams, 1))))))
+"""
+
+def yield_stream(stream):
+    if isinstance(stream, GeneratorType):
+        return yield_stream(next(stream))
+    else:
+        return stream
+
+
+def is_generator_contained(sequence):
+    if isinstance(sequence, GeneratorType):
+        return 
+    
+    if len(sequence) == 0:
+        return False
+
+    if isinstance(sequence[0], GeneratorType):
+        return True
+    else:
+        return is_generator_contained(sequence[1:])
+
+def yield_sequence(sequence):
+    def operate(s):
+        return tuple(map(lambda e:
+            next(e) if isinstance(e, GeneratorType)
+            else e, s))
+    
+    if is_generator_contained(sequence):
+        return yield_sequence(operate(sequence))
+    else:
+        return sequence
+
+
+"""
+For infinite stream
+"""
+def stream_map(f, argstreams):
+    def inner(f, argstreams):
+        if not isinstance(argstreams[0], tuple):
+            return apply(f, argstreams)
+        else:
+            yield cons_stream(
+                apply(f, nth_elements_extractor(0, argstreams)),
+                stream_map(f, yield_sequence(
+                            nth_elements_extractor(1, argstreams))))
+    return inner(f, yield_sequence(argstreams))
