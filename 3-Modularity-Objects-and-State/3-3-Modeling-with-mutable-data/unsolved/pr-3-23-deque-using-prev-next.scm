ee0fe79c818255da52593b86592ccb08d21be6ef
wikibootup <wikibootup@gmail.com>
2015-11-01 23:51:44 +0900

    tdd : print-deque, rear-insert-deque! (partially implemented)

--- 3-Modularity-Objects-and-State/3-3-Modeling-with-mutable-data/unsolved/pr-3-23-deque-using-prev-next.scm
+++ 3-Modularity-Objects-and-State/3-3-Modeling-with-mutable-data/unsolved/pr-3-23-deque-using-prev-next.scm
@@ -22,33 +22,18 @@
 
 
 (load "../../modules/testing.scm")
-;; Initialize and give a name to a simple testsuite.
-(test-begin "test-deque")
+(load "../../modules/2/list-operation.ss")
 
-(define dq0 (make-deque))
-(define elem0 'a)
-
-(define (test-make-deque-consist-of-null deque)
-  ;; empty deque consists of the empty(null) pair
-  (test-assert (and (null? (car deque))
-					(null? (cdr deque)))))
-(test-make-deque-consist-of-null dq0)
-
-(define (test-rear-insert-deque! deque)
-  (define (insert-when-empty element)
-	;; When you insert an element in an empty deque,
- 	;; it should be added inside the deque
-	;; the shape is ([element] ('() '()))
-	(test-assert (eq? element (car (rear-insert-deque! deque element)))))
-  (insert-when-empty elem0))
-(test-rear-insert-deque! dq0)
-
-(define (test-empty-deque-contains-nothing deque)
-  (test-assert (empty-deque? deque)))
-(test-empty-deque-contains-nothing dq0)
-
-(test-end "test-deque")
-
+(define (is-same-lists l1 l2)
+  ;; are l1 & l2 same lists? (pre-requisites : 1-dimension, same length)
+  (cond ((and (null? l1) (null? l2))
+		 true)
+		((and (number? l1) (number? l2))
+		 (= l1 l2))
+		(else
+		 (if (not (= (car l1) (car l2)))
+			 false
+			 (is-same-lists (cdr l1) (cdr l2))))))
 
 (define (make-deque)
   (cons '() '()))
@@ -58,8 +43,54 @@
 
 (define (rear-insert-deque! deque item)
   (let ((new-deque (cons item (cons '() '()))))
-  	(cond ((empty-deque? deque)
-		   (set-car deque new-deque)
-		   (set-cdr deque new-deque))
+	(cond ((empty-deque? deque)
+		   (set-car! deque new-deque)
+		   (set-cdr! deque new-deque))
 		  (else
-		   (display "not yet")))))
+		   (display "not yet"))))))
+
+(define (print-deque deque)
+  (car deque))
+
+
+;; Initialize and give a name to a simple testsuite.
+(test-begin "test-deque")
+
+(define dq0 (make-deque))
+(define dq1 (rear-insert-deque! (make-deque) 1))
+(define elem0 'a)
+
+(define (test-make-deque-consist-of-null deque)
+  ;; empty deque consists of the empty(null) pair
+  (test-assert (and (null? (car deque))
+					(null? (cdr deque)))))
+(test-make-deque-consist-of-null dq0)
+
+(define (test-rear-insert-deque! deque)
+  (define (insert!-when-empty element)
+	;; When you insert an element in an empty deque,
+ 	;; it should be added inside the deque
+	;; the shape is ([element] ('() '()))
+	;;(test-assert true))
+	(rear-insert-deque! deque element)	
+	(test-assert (eq? element (caar deque))))
+  (insert!-when-empty elem0))
+(test-rear-insert-deque! dq0)
+
+(define (test-empty-deque-contains-nothing deque)
+  (test-assert (empty-deque? deque)))
+(test-empty-deque-contains-nothing (make-deque))
+
+(define (test-is-same-lists)
+  (define l1 (cons 1 2))
+  (define l2 (cons 3 4))
+  (test-assert (not (is-same-lists l1 l2)))
+  (test-assert (is-same-lists l1 l1)))
+(test-is-same-lists)
+
+(define (test-print-deque deque)
+  (test-assert (is-same-lists (car deque) (print-deque deque))))
+(define deque-for-print (cons (list 1 2 3 4) (cons '() '())))
+(test-print-deque deque-for-print)
+
+(test-end "test-deque")
