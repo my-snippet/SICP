11be7be1c4d3ab0c4226180f8f84745ecf4f0773
wikibootup <wikibootup@gmail.com>
2015-11-01 20:39:20 +0900

    basic test : for make-deque empty deque

--- 3-Modularity-Objects-and-State/3-3-Modeling-with-mutable-data/unsolved/pr-3-23-deque-using-prev-next.scm
+++ 3-Modularity-Objects-and-State/3-3-Modeling-with-mutable-data/unsolved/pr-3-23-deque-using-prev-next.scm
@@ -20,54 +20,46 @@
 ;; 2-3. Point Tail Node
 ;; (cdr D) -> (I_1)
 
-;;(define (test-make-deque)
-;;  ((make-deque))
-
-(define (make-deque)
-  (cons '() '()))
-
-
-
-(define (test-deque)
-  (define dq0 (make-deque))
-  (define elem0 'a)
-
-  (define (test-make-deque-consist-of-null deque)
-	;; empty deque consists of the empty(null) pair
-	(assert (and (null? (car deque))
-				 (null? (cdr deque)))))
-  (test-make-deque-consist-of-null dq0)
-
-  ;;  (define (test-rear-insert-deque! deque)
-  ;;	(define (insert-when-empty element)
-  ;; When you insert an element in an empty deque,
-  ;; it should be added inside the deque
-  ;; the shape is ([element] ('() '()))
-  ;;(if (eq? 'a (car (insert-rear-deque deque))
-  ;;	  (assert (car (rear-insert-deque! deque element))
-  ;;	  )
-  ;;	(insert-when-empty elem0)
-  
-  ;;	  )
-  )
-  
-(test-deque)
-
 
 (load "../../modules/testing.scm")
 ;; Initialize and give a name to a simple testsuite.
 (test-begin "test-deque")
 
-(define (test-deque)
-  (define dq0 (make-deque))
-  (define elem0 'a)
+(define dq0 (make-deque))
+(define elem0 'a)
 
-  (define (test-make-deque-consist-of-null deque)
-	;; empty deque consists of the empty(null) pair
-	(test-assert (and (null? (car deque))
-					  (null? (cdr deque)))))
-  (test-make-deque-consist-of-null dq0))
+(define (test-make-deque-consist-of-null deque)
+  ;; empty deque consists of the empty(null) pair
+  (test-assert (and (null? (car deque))
+					(null? (cdr deque)))))
+(test-make-deque-consist-of-null dq0)
 
-(test-deque)
+(define (test-rear-insert-deque! deque)
+  (define (insert-when-empty element)
+	;; When you insert an element in an empty deque,
+ 	;; it should be added inside the deque
+	;; the shape is ([element] ('() '()))
+	(test-assert (eq? element (car (rear-insert-deque! deque element)))))
+  (insert-when-empty elem0))
+(test-rear-insert-deque! dq0)
+
+(define (test-empty-deque-contains-nothing deque)
+  (test-assert (empty-deque? deque)))
+(test-empty-deque-contains-nothing dq0)
 
 (test-end "test-deque")
+
+
+(define (make-deque)
+  (cons '() '()))
+
+(define (empty-deque? deque)
+  (null? (car deque)))
+
+(define (rear-insert-deque! deque item)
+  (let ((new-deque (cons item (cons '() '()))))
+  	(cond ((empty-deque? deque)
+		   (set-car deque new-deque)
+		   (set-cdr deque new-deque))
+		  (else
+		   (display "not yet")))))
