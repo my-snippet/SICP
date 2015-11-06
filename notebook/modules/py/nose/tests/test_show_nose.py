a8df35bad62e04ddb0c88fe08144d195310f60d7
wikibootup <wikibootup@gmail.com>
2015-11-06 15:52:42 +0900

    issue infinite recursion nose tests

---
+++ notebook/modules/py/nose/tests/test_show_nose.py
@@ -0,0 +1,14 @@
+from ..show_nose import show_all_test
+import unittest
+
+
+"""
+Some Problem occured. test nose using nose makes recursion each other..
+"""
+"""
+class TestShowNose(unittest.TestCase):
+
+    def test_show_all_tests_contain_something(self):
+        show_nose.show_all_tests()
+        pass
+"""
