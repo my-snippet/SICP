adb2622a904d83481e6908248b03b2e03aeb731d
wikibootup <wikibootup@gmail.com>
2015-11-07 02:18:58 +0900

    Queue init

--- notebook/py/queue/queue.py
+++ notebook/py/queue/queue.py
@@ -1,2 +1,17 @@
-def aa():
-    return 0
+class Queue(object):
+    """An implementation of a simple queue with linked list."""
+
+    def __init__(self):
+        """Initialize an empty queue."""
+        self._first = None
+        self._last = None
+        self._size = 0
+
+    def isEmpty(self):
+        """Check if the queue is empty.
+
+        Returns:
+            True if the queue is empty.
+            False otherwise.
+        """
+        return self._size == 0
