d181b02e004d4a0cdcc9044e4ab1958088190908
wikibootup <wikibootup@gmail.com>
2015-11-10 12:08:23 +0900

    test requirements : test_insert_queue

--- notebook/py/queue/tests/test_queue.py
+++ notebook/py/queue/tests/test_queue.py
@@ -10,11 +10,19 @@ class TestQueue(unittest.TestCase):
     def test_make_queue(self):
         """
         Initial Queue
-        1. first node, second node is initialized to 'None'
-        2. size should be zero. ( isEmpty() == True )
+        1) First node, second node is initialized to 'None'
+        2) Size should be zero. ( isEmpty() == True )
         """
         self.assertIsNone(self.first_queue._first)
         self.assertIsNone(self.first_queue._last)
         self.assertTrue(self.first_queue.isEmpty())
 
-#    def test_
+    def test_insert_queue(self):
+        """
+        1. Empty
+            1) First node should be equal to second node
+        2. Not empty
+            1) Last node should contaions inserted item
+            2) First node should access next node
+        """
+        # pass
