99c9c6962797a45496eb4c5b01f7c31100a27a97
wikibootup <wikibootup@gmail.com>
2015-11-11 00:57:08 +0900

    tdd: test_dequeue

--- notebook/py/queue/tests/test_queue.py
+++ notebook/py/queue/tests/test_queue.py
@@ -37,5 +37,22 @@ class TestQueue(unittest.TestCase):
         self.assertEqual(self.queue._first.item, self.item1)
         self.assertEqual(self.queue._first.next.item, self.queue._last.item)
 
-    #def test_dequeue(self):
+    def test_dequeue(self):
+        """
+        1) If empty, Error should occur
+        2) If just 1 item contained, 
+            2-1) It should be changed to Empty Queue
+            2-2) Head node item should be returned
+        3) If more than 2 items contained, 
+            the second node should contain head node item
+        """
+        self.assertRaises(IndexError, self.queue.dequeue)
+        
+        self.queue.enqueue(self.item1)
+        self.queue.enqueue(self.item2)
+        self.assertEqual(self.queue.dequeue(), self.item1)
+        self.assertEqual(self.queue._first.item, self.item2)
 
+        self.queue.dequeue()
+        self.assertIsNone(self.queue._first)
+        self.assertIsNone(self.queue._last)
