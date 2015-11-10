b62ba282d764aaaa6a964a219b2950056b88ec1f
wikibootup <wikibootup@gmail.com>
2015-11-10 16:16:13 +0900

    test enqueue : when empty

--- notebook/py/queue/tests/test_queue.py
+++ notebook/py/queue/tests/test_queue.py
@@ -4,7 +4,7 @@ from py.queue.queue import Queue
 
 class TestQueue(unittest.TestCase):
     def setUp(self):
-        self.first_queue = Queue()
+        pass
 
     # @unittest.skip("test")
     def test_make_queue(self):
@@ -13,16 +13,23 @@ class TestQueue(unittest.TestCase):
         1) First node, second node is initialized to 'None'
         2) Size should be zero. ( isEmpty() == True )
         """
-        self.assertIsNone(self.first_queue._first)
-        self.assertIsNone(self.first_queue._last)
-        self.assertTrue(self.first_queue.isEmpty())
+        queue = Queue()
 
-    def test_insert_queue(self):
+        self.assertIsNone(queue._first)
+        self.assertIsNone(queue._last)
+        self.assertTrue(queue.isEmpty())
+
+    def test_enqueue(self):
         """
         1. Empty
-            1) First node should be equal to second node
+            1) First node should be equal to last node
         2. Not empty
             1) Last node should contaions inserted item
             2) First node should access next node
         """
         # pass
+        queue = Queue()
+        item1, item2 = 'a', 'b'
+        queue.insert_queue(item1)
+        self.assertEqual(queue._first, item1)
+        self.assertEqual(queue._first, queue._last)
