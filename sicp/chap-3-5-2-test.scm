a7fb4ebdb53f3b49798a1c7e6c6a0d2fa2599abe
wikibootup <wikibootup@gmail.com>
2015-11-26 16:32:45 +0900

    import prime-table.scm

--- sicp/chap-3-5-2-test.scm
+++ sicp/chap-3-5-2-test.scm
@@ -1,6 +1,19 @@
 (load "chap-3-5-2.scm")
+
+;;;; internal lib
+(load "lib/3/stream/stream.scm")
+
+;;;; external lib
 (load "lib/test/chicken-scheme-test.scm")
+;; prime data
+(load "data/prime-table.scm")
+
 
 (test-begin "test-sieve-of-eratosthenes")
+
+(test-assert "Just test" (= 1 1))
+
+;;(test-assert "2" (list "123"))
+
 
 (test-end "test-sieve-of-eratosthenes")
