11901a759c24bb97dcdc4c0733f8ce9bb318d2dd
wikibootup <wikibootup@gmail.com>
2015-11-29 00:52:10 +0900

    add foldr & test

--- sicp/chap-2-2-2-test.scm
+++ sicp/chap-2-2-2-test.scm
@@ -5,11 +5,13 @@
 
 
 (test-begin "test foldl")
-
-(test-assert "Just test" (= 1 1))
-
 (define sample-list (list 1 2 3 4 5))
 (test-assert "sum test" (= (+ 1 2 3 4 5)
 						   (foldl (lambda (a b) (+ a b)) 0 sample-list)))
-
 (test-end "test foldl")
+
+(test-begin "test foldr")
+(define sample-list (list 1 2 3 4 5))
+(test-assert "sum test" (= (+ 1 2 3 4 5)
+						   (foldr (lambda (a b) (+ a b)) 0 sample-list)))
+(test-end "test foldr")
--- sicp/chap-2-2-2.scm
+++ sicp/chap-2-2-2.scm
@@ -2,3 +2,8 @@
   (if (null? xs)
 	  initial
 	  (f (car xs) (foldl f initial (cdr xs)))))
+
+(define (foldr f initial xs)
+  (if (null? xs)
+	  initial
+	  (f (foldr f initial (cdr xs)) (car xs))))
