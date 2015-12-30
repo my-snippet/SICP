1fb0602bfa49b7a1636638d7df2155ce9561f47d
wikibootup <wikibootup@gmail.com>
2015-12-30 15:15:45 +0900

    add rule example

--- sicp/chap-4-4-1.scm
+++ sicp/chap-4-4-1.scm
@@ -40,3 +40,13 @@
 (rule (wheel ?person)
 	  (and (supervisor ?middle-manager ?person)
 		   (supervisor ?x ?middle-manager)))
+
+;; each people lives in same town.
+(rule (lives-near ?person-1 ?person-2)
+	  (and (address ?person-1 (?town . ?rest-1))
+		   (address ?person-2 (?town . ?rest-2))
+		   (not (same ?person-1 ?person-2))))
+
+(rule (same ?x ?x))
+
+(lives-near ?x (Bitdiddle ben))
