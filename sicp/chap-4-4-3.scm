5da56eb6c4188bb0c13453fe3aeae4b4a5c92d13
wikibootup <wikibootup@gmail.com>
2016-01-05 20:32:56 +0900

    add comment : necessity of query order

---
+++ sicp/chap-4-4-3.scm
@@ -0,0 +1,20 @@
+(load "lib/query/ch4-query.scm")
+
+
+(initialize-data-base exercise-db)
+(query-driver-loop)
+
+
+;;;; the necessity of order of query
+(and (job ?x (computer programmer))
+	 (supervisor ?x ?y))
+;;or
+
+(and (supervisor ?x ?y)
+	 (job ?x (computer programmer)))
+;;If a company has many more supervisors than programmers (the usual case),
+;;it is better to use the first form rather than the second because
+;;the data base must be scanned for each intermediate result (frame)
+;;produced by the first clause of the and.
+;;;;
+
