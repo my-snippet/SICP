89b765c2f6e657b7981aed77d9adb4add100d97b
wikibootup <wikibootup@gmail.com>
2016-01-07 18:00:38 +0900

    intro ex-5-3 : original code

---
+++ sicp/ex-5-3.scm
@@ -0,0 +1,13 @@
+(load "lib/register/ch5-regsim")
+
+
+(define (sqrt x)
+  (define (good-enough? guess)
+	(< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+	(average guess (/ x guess)))
+  (define (sqrt-iter guess)
+	(if (good-enough? guess)
+		guess
+		(sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
