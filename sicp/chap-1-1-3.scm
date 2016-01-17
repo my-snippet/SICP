c2a895d90c40f26a949cbfebdfea0bf956c922fc
wikibootup <wikibootup@gmail.com>
2016-01-17 21:41:15 +0900

    in progress chap-1-1-3 : real iterative proc is needed

---
+++ sicp/chap-1-1-3.scm
@@ -0,0 +1,19 @@
+;; Reference
+;; http://c2.com/cgi/wiki?SicpIterationExercise
+
+(define (count-change amount)
+    (cc-new amount 5 0))
+
+(define (cc-new amount kinds-of-coins count)
+  (cond ((= amount 0) (+ count 1))
+		((or (< amount 0) (= kinds-of-coins 0)) count)
+		(else (cc-iter (- amount (first-denomination kinds-of-coins))
+					   kinds-of-coins
+					   (cc-iter amount (- kinds-of-coins 1) count)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+		((= kinds-of-coins 2) 5)
+		((= kinds-of-coins 3) 10)
+		((= kinds-of-coins 4) 25)
+		((= kinds-of-coins 5) 50)))
