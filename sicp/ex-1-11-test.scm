d292be2e10c09a73cb3069dc26d670f9b6571a5b
wikibootup <wikibootup@gmail.com>
2016-01-18 16:39:21 +0900

    in progress ex-1-11 : complete iterative

--- sicp/ex-1-11-test.scm
+++ sicp/ex-1-11-test.scm
@@ -19,3 +19,21 @@
 					 (+ (f 6) (* 2 (f 5)) (* 3 (f 4))))))
 
 (test-end "value test(recursive)")
+
+
+(test-begin "value test(iterative)")
+
+(test-assert "if n<3, then f-iterate(n)=n"
+			 (and (= (f-iterate (- 10)) (- 10))
+				  (= (f-iterate 0) 0)
+				  (= (f-iterate 2) 2)))
+
+(test-assert "f-iterate(4) = f-iterate(4-1) + 2f-iterate(4-2) + 3f-iterate(4-3)"
+			 (and (= (f-iterate 4)
+					 (+ (f-iterate (- 4 1)) (* 2 (f-iterate (- 4 2))) (* 3 (f-iterate (- 4 3)))))))
+
+(test-assert "f-iterate(7) = f-iterate(6) + 2f-iterate(5) + 3f-iterate(4)"
+			 (and (= (f-iterate 7)
+					 (+ (f-iterate 6) (* 2 (f-iterate 5)) (* 3 (f-iterate 4))))))
+
+(test-end "value test(iterative)")
--- sicp/ex-1-11.scm
+++ sicp/ex-1-11.scm
@@ -3,3 +3,11 @@
 		(else (+ (f (- n 1))
 				 (* 2 (f (- n 2)))
 				 (* 3 (f (- n 3)))))))
+
+
+;; reference
+;; http://community.schemewiki.org/?sicp-ex-1.11
+(define (f-iterate n)
+  (define (f-iter count n f1 f2 f3)
+	(if (= count n) f3 (f-iter (+ count 1) n f2 f3 (+ (* 3 f1) (* 2 f2) f3))))
+  (if (<= n 3) n (f-iter 3 n 1 2 3)))
