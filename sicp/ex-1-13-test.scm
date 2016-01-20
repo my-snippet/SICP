667571ebffaab126734452627f65c765bd651bef
wikibootup <wikibootup@gmail.com>
2016-01-20 16:29:09 +0900

    refactor ex-1-13 : seperate test & libraries

--- sicp/ex-1-13-test.scm
+++ sicp/ex-1-13-test.scm
@@ -7,23 +7,6 @@
 
 (test-begin "fib n test")
 
-(define phi (/ (+ 1 (sqrt 5)) 2))
-
-(define (gradually-decreasing f1 f2 n)
-  (define (gd-iter k previous-result)
-	(if (> k n)
-		true
-		(let ((now-result (abs (- (f1 k) (f2 k)))))
-		  (let ((decreased? (> previous-result now-result)))
-			(display now-result)
-			(newline)
-			(if decreased?							   
-				(gd-iter (+ k 1) now-result)
-				(error
-				 "It has been decreased from 0 to" (- k 1) ", not decreased from" k
-				 ))))))
-  (gd-iter 0 1))
-
 (test-assert "prove fib n value is approximately golden ratio when n is increasing"
 			 ;; if n is over 35, almost double times increased,
 			 ;; it is because data is may too small.
--- sicp/lib/basic/operations.scm
+++ sicp/lib/basic/operations.scm
@@ -21,3 +21,20 @@
 
 (define (combination n k)
   (/ (permutation n k) (factorial k)))
+
+(define phi (/ (+ 1 (sqrt 5)) 2))
+
+(define (gradually-decreasing f1 f2 n)
+  (define (gd-iter k previous-result)
+	(if (> k n)
+		True
+		(let ((now-result (abs (- (f1 k) (f2 k)))))
+		  (let ((decreased? (> previous-result now-result)))
+			(display now-result)
+			(newline)
+			(if decreased?							   
+				(gd-iter (+ k 1) now-result)
+				(error
+				 "It has been decreased from 0 to" (- k 1) ", not decreased from" k
+				 ))))))
+  (gd-iter 0 1))
