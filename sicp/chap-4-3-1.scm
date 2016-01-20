1222c3fd8e90e618e36f1c41a21d03aca7599f9c
wikibootup <wikibootup@gmail.com>
2016-01-20 20:28:44 +0900

    in progress chap4-3-1 : add warning

--- sicp/chap-4-3-1.scm
+++ sicp/chap-4-3-1.scm
@@ -1,8 +1,9 @@
-(load "lib/amb/ch4-ambeval.scm")
-
-
-(driver-loop)
+;;;;**WARNING: Don't load this file twice (or you'll lose the primitives
+;;;;  interface, due to renamings of apply).
+;;(load "lib/eval/ch4-ambeval.scm")
+;;(driver-loop)
 
 
 (amb 'a 'b)
 (amb '1 '2 '3)
+(cons 1 2)
