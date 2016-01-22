1a7552ebc4477c297221e9117e64f7420e274eab
wikibootup <wikibootup@gmail.com>
2016-01-22 17:44:45 +0900

    complete ex-1-16

---
+++ sicp/ex-1-16-test.scm
@@ -0,0 +1,22 @@
+(load "ex-1-16.scm")
+
+;;;; external lib
+(load "lib/test/chicken-scheme-test.scm")
+
+(test-begin "fast-expt-iter test")
+
+(test-assert "2^0 = 1"
+			 (= (fast-expt 2 0) 1))
+
+(test-assert "2^1 = 2"
+			 (= (fast-expt 2 1) 2))
+
+(test-assert "2^9 = 512"
+			 (= (fast-expt 2 9) 512))
+
+(test-assert "2^10 = 1024"
+			 (= (fast-expt 2 10) 1024))
+
+(test-end "fast-expt-iter test")
+(map (lambda (n) (fast-expt 2 n)) (list 1 2 3 4 5 10 11 12 13 14 15 20 21 22 23 24 25))
+
---
+++ sicp/ex-1-16.scm
@@ -0,0 +1,10 @@
+(define (fast-expt b n)
+  ;; It starts from n-count-rev = n, and ends when n-count-rev = 1
+  ;; even-value should starts from b because when (square 1), it cannot be calculated.
+  (define (fast-expt-iter even-value odd-value n-count-rev)
+	(cond ((= n-count-rev 0) 1)
+		  ((= n-count-rev 1) (* even-value odd-value))
+		  ((= (remainder n-count-rev 2) 0)
+		   (fast-expt-iter (square even-value) odd-value (/ n-count-rev 2)))
+		  (else (fast-expt-iter even-value (* odd-value even-value) (- n-count-rev 1)))))
+  (fast-expt-iter b 1 n))
