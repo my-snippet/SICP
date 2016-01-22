6994dbca06c307290bea3339fa7bf250c7aa27ad
wikibootup <wikibootup@gmail.com>
2016-01-22 17:55:20 +0900

    refactor ex-1-16

--- sicp/ex-1-16-test.scm
+++ sicp/ex-1-16-test.scm
@@ -19,3 +19,20 @@
 
 (test-end "fast-expt-iter test")
 (map (lambda (n) (fast-expt 2 n)) (list 1 2 3 4 5 10 11 12 13 14 15 20 21 22 23 24 25))
+
+(test-begin "fast-expt-iter2 test")
+
+(test-assert "2^0 = 1"
+			 (= (fast-expt2 2 0) 1))
+
+(test-assert "2^1 = 2"
+			 (= (fast-expt2 2 1) 2))
+
+(test-assert "2^9 = 512"
+			 (= (fast-expt2 2 9) 512))
+
+(test-assert "2^10 = 1024"
+			 (= (fast-expt2 2 10) 1024))
+
+(test-end "fast-expt-iter2 test")
+(map (lambda (n) (fast-expt2 2 n)) (list 1 2 3 4 5 10 11 12 13 14 15 20 21 22 23 24 25))
--- sicp/ex-1-16.scm
+++ sicp/ex-1-16.scm
@@ -11,3 +11,12 @@
 		   (fast-expt-iter (square even-value) odd-value (/ n-count-rev 2)))
 		  (else (fast-expt-iter even-value (* odd-value even-value) (- n-count-rev 1)))))
   (fast-expt-iter b 1 n))
+
+;; http://www.billthelizard.com/2010/01/sicp-exercise-116-fast-exponentiation.html
+;; corrected version
+(define (fast-expt2 b n)
+  (define (fast-expt-iter b n a)
+	(cond ((= n 0) a)
+		  ((even? n) (fast-expt-iter (square b) (/ n 2) a))
+		  (else (fast-expt-iter  b (- n 1) (* a b)))))
+  (fast-expt-iter b n 1))
