c1ee6f709d2ea89ec0ce4d2b48395ddae777e9cf
wikibootup <wikibootup@gmail.com>
2016-01-23 14:56:00 +0900

    complete ex-1-19

---
+++ sicp/ex-1-19-test.scm
@@ -0,0 +1,5 @@
+(load "ex-1-19.scm")
+
+
+(map (lambda (n) (fib-advanced n))
+	 (list 1 2 3 4 5 10 11 12 13 14 15 50 51 52 53 54 55))
---
+++ sicp/ex-1-19.scm
@@ -0,0 +1,24 @@
+;; reference
+;; http://sarabander.github.io/sicp/html/1_002e2.xhtml#g_t1_002e2_002e4
+
+(define (fib-advanced n)
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0)
+		 b)
+		((even? count)
+		 (fib-iter a
+				   b
+				   (+ (square p) (square q))
+				   (+ (square q) (* 2 p q))
+				   (/ count 2)))
+		(else
+		 (fib-iter (+ (* b q)
+					  (* a q)
+					  (* a p))
+				   (+ (* b p)
+					  (* a q))
+				   p
+				   q
+				   (- count 1)))))
