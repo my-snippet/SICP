5d6bc0367ce8a60f553a9d4a0d91578a8745d94d
wikibootup <wikibootup@gmail.com>
2016-01-24 22:41:18 +0900

    refactor ex-1-17 : add recursive style proc

--- sicp/ex-1-17-test.scm
+++ sicp/ex-1-17-test.scm
@@ -4,7 +4,7 @@
 (load "lib/test/chicken-scheme-test.scm")
 
 
-(test-begin "multiplication advanced test")
+(test-begin "*_ test (iterative style multiplication)")
 
 (test-assert "17 *_ 0 = 0 *_ 17 = 0"
 			 (= (*_ 17 0) (*_ 0 17) 0))
@@ -12,4 +12,15 @@
 (test-assert "(17 *_ 17) - (17 *_ 16) = 17"
 			 (= (- (*_ 17 17) (*_ 17 16)) 17))
 
-(test-end "multiplication advanced test")
+(test-end "*_ test (iterative style multiplication)")
+
+
+(test-begin "*-recursive test (recursive style multiplication)")
+
+(test-assert "17 *-recursive 0 = 0 *-recursive 17 = 0"
+			 (= (*-recursive 17 0) (*-recursive 0 17) 0))
+
+(test-assert "(17 *-recursive 17) - (17 *-recursive 16) = 17"
+			 (= (- (*-recursive 17 17) (*-recursive 17 16)) 17))
+
+(test-end "*-recursive test (recursive style multiplication)")
--- sicp/ex-1-17.scm
+++ sicp/ex-1-17.scm
@@ -8,3 +8,8 @@
 		  ((even? b) (*_-iter (double a) (halve b) result))
 		  (else (*_-iter a (- b 1) (+ result a)))))
   (*_-iter a b 0))
+
+(define (*-recursive a b) 
+  (cond ((= b 0) 0)
+		((even? b) (+ (double a) (*-recursive (double a) (halve b))))
+		(else (+ a (*-recursive a (- b 1))))))
