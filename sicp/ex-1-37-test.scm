3f427ad3c404c1a4ad085800f704eade65aa0f94
wikibootup <wikibootup@gmail.com>
2016-01-31 18:26:10 +0900

    complete ex-1-37

---
+++ sicp/ex-1-37-test.scm
@@ -0,0 +1,17 @@
+(load "ex-1-37.scm")
+(load "lib/basic/operations.scm")
+
+
+(find-n-when-satisfy-tolerance
+ (lambda (k) (cont-frac-recur (lambda (i) 1.0)
+							  (lambda (i) 1.0)
+							  k))
+ (/ 1 1.6180327868852458) 0.0001 1 inc)
+
+(find-n-when-satisfy-tolerance
+ (lambda (k) (cont-frac-iter (lambda (i) 1.0)
+							  (lambda (i) 1.0)
+							  k))
+ (/ 1 1.6180327868852458) 0.0001 1 inc)
+
+(/ 1 1.6180327868852458)
---
+++ sicp/ex-1-37.scm
@@ -0,0 +1,25 @@
+;; old version
+;;(define (cont-frac-recur n d k)
+;;  (if (= k 0)
+;;	  0
+;;	  (/ (n 'dummy) (+ (d 'dummy)
+;;					   (cont-frac-recur n d (- k 1))))))
+
+
+;; reference : http://community.schemewiki.org/?sicp-ex-1.37
+;; applying i version
+;; pay attention to 'predicate'
+(define (cont-frac-recur n d k)
+  (define (recur i)
+	(if (> i k)
+		0
+		(/ (n i) (+ (d i)
+					(recur (+ i 1))))))
+  (recur 1))
+
+(define (cont-frac-iter n d k)
+  (define (iter i result)
+	(if (= i 0)
+		result
+		(iter (+ i 1) (/ (n i) (+ (d i) result)))))
+  (iter 1 k))
