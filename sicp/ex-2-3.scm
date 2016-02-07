e05ee97710952e8ed99dd64dd91b2d28d16ae640
wikibootup <wikibootup@gmail.com>
2016-02-08 00:24:18 +0900

    add implementation design ex-2-3

---
+++ sicp/ex-2-3.scm
@@ -0,0 +1,6 @@
+(load "ex-2-2.scm")
+
+
+;; If it takes two segments as the arguments to create a rectangle,
+;; it will be more complex ( Direction, start and end points, etc. ).
+;; Therefore here it takes 4 points as the arguments
