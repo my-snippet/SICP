32d18e38cef70440235db955960e416a0114b2a3
wikibootup <wikibootup@gmail.com>
2016-02-11 21:00:59 +0900

    complete ex-2-8

--- sicp/ex-2-8.scm
+++ sicp/ex-2-8.scm
@@ -1,5 +1,18 @@
-;; idea
+;; idea 1
 ;; interval of a : a'
 ;; interval of b : b'
 ;; min(a'-b') or min(b'-a') = min(a,b) - max(a,b)
 ;; max(a'-b') or max(b'-a') = max(a,b) - min(a,b)
+
+;; * note
+;; but idea 1 is not correct. because, interval procedure will always
+;; be executed from a to b ( not b to a )
+
+(load "chap-2-1-4.scm")
+
+
+(define (sub-interval x y)
+  (make-interval (- (upper-bound x)
+					(lower-bound y))
+				 (- (lower-bound x)
+					(upper-bound y))))
