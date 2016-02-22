7bebe82a53b02c1a3bd8fa555838df8292757eba
wikibootup <wikibootup@gmail.com>
2016-02-22 22:35:00 +0900

    complete ex-2-9

--- sicp/ex-2-9-test.scm
+++ sicp/ex-2-9-test.scm
@@ -10,6 +10,7 @@
 
 (define i1 (make-interval (- 3) 3))
 (define i2 (make-interval 0 9))
+(define i3 (make-interval 0 6))
 
 
 (test-assert
@@ -24,4 +25,11 @@ sum of the width of each interval"
  (= (width (sub-interval i1 i2))
 	(+ (width i1) (width i2))))
 
+(test-assert
+ "width of i1 and i3 should be equal"
+	(= (width i1) (width i3)))
+
+;; In case of Multiplication, Dvision, there are sign problem
+;; For example, width(-2, -1) = (-1) - (-2) / 2 != width(-1, 0) = (-1) - 0 / 2 
+;; And refer to mul-interval procedure in Chap-2-1-4, min max problem also exsit because of sign
 (test-end "width test")
