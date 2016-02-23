935022e339cde37ea2cb9a66310c2b5e15162137
wikibootup <wikibootup@gmail.com>
2016-02-24 00:55:04 +0900

    complete ex-2-11

--- sicp/ex-2-11-test.scm
+++ sicp/ex-2-11-test.scm
@@ -1,7 +1,35 @@
 (load "ex-2-7.scm")
 (load "ex-2-11.scm")
 
+;;;; external lib
+(load "lib/test/chicken-scheme-test.scm")
 
-(define i1 (make-interval (- 3) 3))
-(define i2 (make-interval 5 8))
-(mul-interval-9-cases i1 i2)
+(test-begin "mul-interval-9-cases test")
+
+(test-assert "[-2 3] * [-2 3] should be [-6 9]"
+			 (let ((npnp (mul-interval-9-cases np np)))
+			   (and (= (- 6) (lower-bound npnp))
+					(= 9 (upper-bound npnp)))))
+
+(test-end "mul-interval-9-cases test")
+
+
+(define n (- 2))
+(define p 3) 
+
+(define nn (make-interval (* 2 n) n))
+(define np (make-interval n p))
+(define pp (make-interval p (* 2 p)))
+
+
+(mul-interval-9-cases pp pp)
+(mul-interval-9-cases pp np)
+(mul-interval-9-cases pp nn)
+
+(mul-interval-9-cases np pp)
+(mul-interval-9-cases np np)
+(mul-interval-9-cases np nn)
+
+(mul-interval-9-cases nn pp)
+(mul-interval-9-cases nn np)
+(mul-interval-9-cases nn nn)
