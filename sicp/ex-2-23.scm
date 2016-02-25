5541ed2266b763927503dbd14d1b4f0e9f52e2fc
wikibootup <wikibootup@gmail.com>
2016-02-25 12:53:21 +0900

    refactor ex2.23 : add comment about begin intruction

--- sicp/ex-2-23.scm
+++ sicp/ex-2-23.scm
@@ -7,6 +7,8 @@
 
 ;; The cond syntax embedded (begin)
 ;; http://sicp.ai.mit.edu/Fall-2003/manuals/scheme-7.5.5/doc/scheme_3.html#SEC39
+;; scheme evaluates only one expression or statements, but 'begin' instruction 
+;; makes the process evaluates all the expressions until it ends.
 (define (for-each p l)
   (cond ((null? l) 'done)
 		(else (p (car l)) (for-each p (cdr l)))))
