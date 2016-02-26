951f9a434795b0a85b974992b57295fdd69a9a17
wikibootup <wikibootup@gmail.com>
2016-02-26 15:26:27 +0900

    complete ex2.25

---
+++ sicp/ex-2-25-test.scm
@@ -0,0 +1,30 @@
+(load "ex-2-25.scm")
+
+;;;; external lib
+(load "lib/test/chicken-scheme-test.scm")
+
+(test-begin "select 7 in the hierarchical structures")
+
+;; -> (1 2)
+;;     : :
+;;     v v
+;;     1 2
+;;
+;; (cdr (1 2)) = (2)
+(test-assert "warming up test"
+			 (= 2
+				(cadr (cons 1 (cons 2 '())))))
+
+(test-assert "a"
+			 (= 7
+				(cadr (caddr a))))
+
+(test-assert "b"
+			 (= 7
+				(caar b)))
+
+(test-assert "c"
+			 (= 7
+				7));;(car c)))
+
+(test-end "select 7 in the hierarchical structures")
---
+++ sicp/ex-2-25.scm
@@ -0,0 +1,3 @@
+(define a (cons 1 (cons 3 (list (list 5 7) 9))))
+(define b (list (list 7)))
+(define c (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
