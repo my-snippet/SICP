f4b03119b9fbccbce1e28efebbd7512721d15f1c
wikibootup <wikibootup@gmail.com>
2016-02-27 00:00:28 +0900

    refactor ex2.28 : code more fit into design patterns

--- sicp/ex-2-28.scm
+++ sicp/ex-2-28.scm
@@ -1,7 +1,7 @@
 ;; The reason why it checks null? : because (list '()) makes nested list -> ('())
 ;; Therefore it makes many '() value in the flat lists
-(define (fringe-version0 x)
-  (cond ((null? x) '())
-		((pair? x) (append (fringe-version0 (car x))
-						   (fringe-version0 (cdr x))))
-		(else (list x))))
+(define (fringe t)
+  (cond ((null? t) '())
+		((not (pair? t)) (list t))
+		(else ((pair? t) (append (fringe (car t))
+								 (fringe (cdr t)))))))
