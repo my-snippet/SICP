e3b29d2ecc85ed9566c7696bd469a0a0300a5545
wikibootup <wikibootup@gmail.com>
2016-02-27 00:21:38 +0900

    complete ex2.28 : add final implementation using map

--- sicp/ex-2-28.scm
+++ sicp/ex-2-28.scm
@@ -1,3 +1,13 @@
+;; This idea(using map) is inspired by ex2.27 
+(define (flat-1d tree)
+  (append (car x) (cadr x)))
+
+(define (fringe tree)  
+  (cond ((null? tree) '())
+		((not (pair? tree)) (list tree))
+		(else (fringe (map flat-1d tree)))))
+
+
 ;; The reason why it checks null? : because (list '()) makes nested list -> ('())
 ;; Therefore it makes many '() value in the flat lists
 (define (fringe-version0 x)
@@ -12,16 +22,16 @@
 ;; It is more fit into design patterns
 ;; it specifies the purpose of argument value as name,
 ;; and it loacates the main process in the else clause.
-(define (fringe tree)
+(define (fringe-version1 tree)
   (cond ((null? tree) '())
 		((not (pair? tree)) (list tree))
-		(else (append (fringe (car tree))
-					  (fringe (cdr tree))))))
+		(else (append (fringe-version1 (car tree))
+					  (fringe-version1 (cdr tree))))))
 
 ;; code reference
 ;; http://community.schemewiki.org/?sicp-ex-2.28
 ;; iterative version
-(define (fringe-version1 T)
+(define (fringe-version2 T)
   (define (iter T R)
 	(cond ((null? T) R)
 		  ((not (pair? T)) (cons T R))
