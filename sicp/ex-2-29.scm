092d03f5d08f4b18e917f393aeff26c2d4e2cf16
wikibootup <wikibootup@gmail.com>
2016-02-29 11:12:20 +0900

    update ex2.29 : constructors with cons

--- sicp/ex-2-29.scm
+++ sicp/ex-2-29.scm
@@ -3,3 +3,10 @@
 
 (define (make-branch length structure)
   (list length structure))
+
+
+(define (make-mobile-cons left right)
+  (cons left right))
+
+(define (make-branch-cons length structure)
+    (cons length structure))
