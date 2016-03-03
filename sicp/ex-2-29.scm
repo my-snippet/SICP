3235c2902ceec4c845347a906f0b26c769fd0430
wikibootup <wikibootup@gmail.com>
2016-03-03 22:19:30 +0900

    refactor ex2.29 : add vector comment

--- sicp/ex-2-29.scm
+++ sicp/ex-2-29.scm
@@ -53,6 +53,7 @@
 
 ;; Avoid the direction problem in the mobile using that the torque
 ;; procedure does not allow a branch that has mobile below it.
+;; * direction problems = vector problems
 (define (branch-torque b)
   (if (pair? (branch-weight b))
 	  (error "Only one level depth branch possible.")
