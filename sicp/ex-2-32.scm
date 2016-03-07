3a99f688b9a4baec8d45d38333db6b185019a0fd
wikibootup <wikibootup@gmail.com>
2016-03-07 16:06:20 +0900

    complete ex2.32

--- sicp/ex-2-32.scm
+++ sicp/ex-2-32.scm
@@ -1,6 +1,5 @@
-;; in progress
 (define (subsets s)
   (if (null? s)
 	  (list '())
 	  (let ((rest (subsets (cdr s))))
-;;		(append rest (map (lambda (x) (append s x)) rest)))))
+		(append rest (map (lambda (x) (cons (car s) x)) rest)))))
