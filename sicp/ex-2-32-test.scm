12e3f688ee616ec2f9ae596e66b69827586b6553
wikibootup <wikibootup@gmail.com>
2016-03-07 12:02:07 +0900

    inprogress ex2.32 : need core statement

---
+++ sicp/ex-2-32-test.scm
@@ -0,0 +1,4 @@
+(load "ex-2-32.scm")
+
+
+(subsets (list 1 2 3))
---
+++ sicp/ex-2-32.scm
@@ -0,0 +1,6 @@
+;; in progress
+(define (subsets s)
+  (if (null? s)
+	  (list '())
+	  (let ((rest (subsets (cdr s))))
+;;		(append rest (map (lambda (x) (append s x)) rest)))))
