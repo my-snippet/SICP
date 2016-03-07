557231c48ff1c4b1649b621253b7343e5a574643
wikibootup <wikibootup@gmail.com>
2016-03-07 16:20:33 +0900

    inprogress ex2.32 : add comment ex2.32

--- sicp/ex-2-32.scm
+++ sicp/ex-2-32.scm
@@ -1,3 +1,20 @@
+;; Anyway, lists like (1 2 nil) only can be made by appending other data.
+;; because selectors for list cannot select (1 2 nil) in (1 2 3 nil)
+;; that is, function in map should append(or cons up) (car s) & other data
+;; or list  
+
+;; The line (let ((rest (subsets (cdr s)))) calls the subsets recursively
+;; until it will be nil,
+;; 1. at that time subsets returns '(), therefore '() will appears first,
+;; 2. and then, 3 and '() will be consed up to be (3)
+;; 3. and then, 2 and '() will be consed up to be (2)
+;; 4. and then, 2 and (3) will be consed up to be (2 3)
+
+;; in progress 
+;; Why 2. 3. happen successively? : because subsets is called when it is
+;; to be '(), it first evaluates 3, and then evaluates 2
+
+
 (define (subsets s)
   (if (null? s)
 	  (list '())
