dbcdf38c27b8f22bd6859889b31fd580305e2eb5
wikibootup <wikibootup@gmail.com>
2016-03-10 10:55:31 +0900

    complete ex2.28 : test the others

--- sicp/ex-2-28-test.scm
+++ sicp/ex-2-28-test.scm
@@ -4,8 +4,17 @@
 (define x (list (list 1 2) (list 3 4)))
 (define y (list (list (list 0 1) 2) (list 3 4)))
 
+x
+y
+
+(fringe x)
+(fringe y)
+
 (fringe-version0 x)
 (fringe-version0 y)
 
-;;(fringe x)
-;;(fringe y)
+(fringe-version1 x)
+(fringe-version1 y)
+
+(fringe-version2 x)
+(fringe-version2 y)
